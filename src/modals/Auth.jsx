21b8ee43c6ca998655afb9c84adfc99d99873307
Hritik Kumar <144543568+hritik095@users.noreply.github.com>
2025-10-05 13:18:06 +0530

    Update Auth.jsx

--- src/modals/Auth.jsx
+++ src/modals/Auth.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { useAppContext } from "../context/appContext";
+import { useAppContext } from "../context/AppContext";
 import { toast } from "react-hot-toast";
 import { FaUserAlt } from "react-icons/fa";
 
